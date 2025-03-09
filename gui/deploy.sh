0f4bd3ec3decc252dba8084066ed59eeded4bd06
haborite <korintje@ymail.ne.jp>
2025-03-09 20:07:45 +0900

    fix deploy sh

--- gui/deploy.sh
+++ gui/deploy.sh
@@ -1,8 +1,16 @@
 #!/bin/sh
 set -eux
+
+# Get version as the argument
+VERSION=${1:-0.4.0}  # デフォルトで0.4.0を使用。引数が渡された場合はそれを使用。
+
+# nuitka compile
 python -m nuitka --standalone --enable-plugin=pyside6 --windows-console-mode=disable --output-dir=linux ku1255-fw-remapper.py
+
 cd linux
-mv ku1255-fw-remapper.dist ku1255-fw-remapper-linux-0.4.0
-cp ../keymaps.csv ku1255-fw-remapper-linux-0.4.0/keymaps.csv
-cp -r ../examples ku1255-fw-remapper-linux-0.4.0/examples
-tar -zcvf ../upload/ku1255-fw-remapper-linux-0.4.0.tar.gz ku1255-fw-remapper-0.4.0
+mv ku1255-fw-remapper.dist "ku1255-fw-remapper-linux-${VERSION}"
+cp ../keymaps.csv "ku1255-fw-remapper-linux-${VERSION}/keymaps.csv"
+cp -r ../examples "ku1255-fw-remapper-linux-${VERSION}/examples"
+
+# Create tar.gz archive
+tar -zcvf "../upload/ku1255-fw-remapper-linux-${VERSION}.tar.gz" "ku1255-fw-remapper-linux-${VERSION}"
